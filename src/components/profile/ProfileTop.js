c8d264805d199620ca5af07cbb1be2553d5234ee
Austin Moore <37962463+Austinmoore1492@users.noreply.github.com>
2020-01-13 09:42:33 -0600

    Updated Profile

--- src/components/profile/ProfileTop.js
+++ src/components/profile/ProfileTop.js
@@ -3,11 +3,11 @@ import React, { Fragment } from "react";
 const ProfileTop = () => (
   <Fragment>
     <div className="profile-top">
-      <img
+      {/* <img
         className="round-img m-1"
         src="https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/58939921_2213967395348994_6457749089227374592_n.jpg?_nc_cat=105&_nc_ohc=Rond-dFksWcAQkeVQPNm42WtAIabhqLEBZCl3hCreAObnq50nnPariNxQ&_nc_ht=scontent-ort2-2.xx&oh=a37c5df8eca6f62977109105a76182cf&oe=5E83CD77"
         alt=""
-      />
+      /> */}
       <div className="profile-info">
         <h1 className="large">Austin Moore</h1>
         <p className="lead">Knowledge Manger/Web Developer</p>
